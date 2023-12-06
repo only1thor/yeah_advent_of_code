a07473ec4181d61848b27362ab078b1f27eea123
only1thor <>
2023-12-06 20:37:57 +0100

    add basic tests

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,26 @@
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+    println!("Test of date result: {}", re.is_match("2020-01-01"));
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Test happy case
+    #[test]
+    fn test_regex() {
+        let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+        assert!(re.is_match("2020-01-01"));
+    }
+
+    // Test sad case
+    #[test]
+    fn test_regex_fail() {
+        let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+        assert_eq!(re.is_match("2020-01-01-01"), false);
+    }
 }
